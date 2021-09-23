ee9d586ab1a38085d202204eeebc43c58611cb91
sudipta-nandan <nandan.sudipta@gmail.com>
2021-09-24 02:43:21 +0530

    fix name of user in bot's greeting msg.

--- main.py
+++ main.py
@@ -7,22 +7,25 @@ import os
 load_dotenv()
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     if message.content.startswith('$hello'):
-        await message.channel.send('Hello {0}!'.format(message.author))
+        await message.channel.send('Hello {0}!'.format(message.author.name))
+
 
 @client.event
 async def on_member_join(member):
     guild = member.guild
     if guild.system_channel is not None:
-        msg = f'Welcome {member.mention} to {guild.name}'
+        msg = f'Hi {member.name}!\nWelcome to {guild.name}'
         await guild.system_channel.send(msg)
 
 client.run(os.getenv('TOKEN'))
