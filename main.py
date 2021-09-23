3eeba54921c4e2e0e5692c1e6da1ba4edbae49a0
omecamtiv <nandan.sudipta@gmail.com>
2021-09-24 00:07:15 +0530

    authored

---
+++ main.py
@@ -0,0 +1,28 @@
+#! /bin/env python3
+
+from dotenv import load_dotenv
+import discord
+import os
+
+load_dotenv()
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello {0}!'.format(message.author))
+
+@client.event
+async def on_member_join(member):
+    guild = member.guild
+    if guild.system_channel is not None:
+        msg = f'Welcome {member.mention} to {guild.name}'
+        await guild.system_channel.send(msg)
+
+client.run(os.getenv('TOKEN'))
